3f7182fb68f1898ab1dbb5906d8b2cdc750cc627
olender.konra <olender.konra@gmail.com>
2021-02-22 11:21:52 +0100

    Dodaj pliki projektów.

---
+++ Bezpieczeństwo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bezpieczeństwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
