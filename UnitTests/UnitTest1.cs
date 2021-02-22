3ff9b34464e534d4d18d32466531d6b30720aca5
Gabrysia Sawicka <99sgabi@wp.pl>
2021-02-22 23:16:17 +0100

    added test

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -127,5 +127,35 @@ namespace UnitTests
             string notCipheredAgain = algorytm.DecipherString(ciphered);
             Assert.AreEqual(notCipheredAgain, notCiphered);
         }
+
+        [Test]
+        public void TestPrzestawieniaMacierzoweAMore()
+        {
+            PrzestawieniaMacierzoweA algorytm = new PrzestawieniaMacierzoweA();
+            string key1 = "9,4,3,1,8,5,7,6,2";
+            string key2 = "4,3,1,9,5,8,7,6,2";
+            string key3 = "8,7,9,5,2,1,3,6,4";
+            string notCiphered = "GABRYSIA";
+            string notCipheredLong = "GABRYSIAGABRYSI";
+            string ciphered1 = "RBGAYISA";
+            string ciphered2 = "RBGYAISA";
+            string cipheredLong = "AIGYAGBSRSBARIY";
+            
+
+            Assert.AreEqual(true, algorytm.PrepareKey(key1, ','));
+            Assert.AreEqual(notCiphered, algorytm.DecipherString(ciphered1));
+            Assert.AreEqual(ciphered1, algorytm.CipherString(notCiphered));
+
+            algorytm.ClearKey();
+            Assert.AreEqual(true, algorytm.PrepareKey(key2, ','));
+            Assert.AreEqual(notCiphered, algorytm.DecipherString(ciphered2));
+            Assert.AreEqual(ciphered2, algorytm.CipherString(notCiphered));
+
+            algorytm.ClearKey();
+            Assert.AreEqual(true, algorytm.PrepareKey(key3, ','));
+            Assert.AreEqual(notCipheredLong, algorytm.DecipherString(cipheredLong));
+            Assert.AreEqual(cipheredLong, algorytm.CipherString(notCipheredLong));
+        }
+
     }
 }
