5b6d08ddbc0ca6e644e0a90fea5f618717b4ff56
ewa <ewcia112211@gmail.com>
2021-02-23 18:43:02 +0100

    przestawieniamacierzoweB - GetKey function

--- Bezpieczeństwo/Algorithms/PrzestawieniaMacierzoweB.cs
+++ Bezpieczeństwo/Algorithms/PrzestawieniaMacierzoweB.cs
@@ -7,9 +7,9 @@ namespace Bezpieczeństwo.Algorithms
 {
     public class PrzestawieniaMacierzoweB
     {
-        public String Cipher(string text)
+        public String Cipher(string text, String userKey)
         {
-            int[] key = GetKey();
+            int[] key = GetKey(userKey);
             char[,] tab = new char[key.Length, (int)text.Length / key.Length + 1];
             String output = "";
             int index = 0;
@@ -41,9 +41,9 @@ namespace Bezpieczeństwo.Algorithms
             return output;
         }
 
-        public String Decipher(String text)
+        public String Decipher(String text, String userKey)
         {
-            int[] key = GetKey();
+            int[] key = GetKey(userKey);
             Console.WriteLine(key.Length);
             String output = "";
             char[,] tab = new char[key.Length, (int)text.Length / key.Length + 1];
@@ -75,11 +75,55 @@ namespace Bezpieczeństwo.Algorithms
         }
 
         //Zwraca tablicę na podstawie słowa klucz
-        public int[] GetKey()
+        /*public int[] GetKey()
         {
             int[] i = new int[] { 1, 6, 2, 4, 8, 3, 5, 7, 9, 10};
             return i; 
+        }*/
+
+        private char[] QuickSort(char[]d, int l, int r)
+        {
+            int i, j;
+            char p, swap;
+
+            i = (l + r) / 2;
+            p = d[i]; d[i] = d[r];
+            for (j = i = l; i < r; i++)
+                if (d[i] < p)
+                {
+                    swap = d[i];
+                    d[i] = d[j];
+                    d[j] = swap;
+                    j++;
+                }
+            d[r] = d[j]; d[j] = p;
+            if (l < j - 1) QuickSort(d, l, j - 1);
+            if (j + 1 < r) QuickSort(d, j + 1, r);
+
+            return d;
         }
+
+        public int[] GetKey(String key)
+        {
+            char[] ckey = new char[key.Length];
+            for (int j = 0; j < key.Length; j++)
+                ckey[j] = key[j];
+
+            int l = 0, r = key.Length - 1;
+            ckey = QuickSort(ckey, l, r);
+
+            int[] i = new int[key.Length];
+            for(int j = 0; j < ckey.Length; j++)
+            {
+                int p = 0;
+                while (ckey[j] != key[p]) p++;
+                i[p] = j+1;
+            }
+            
+            return i;
+        }
+
+
     }
 }
 
--- Bezpieczeństwo/Controllers/HomeController.cs
+++ Bezpieczeństwo/Controllers/HomeController.cs
@@ -22,8 +22,10 @@ namespace Bezpieczeństwo.Controllers
         public IActionResult Index()
         {
             PrzestawieniaMacierzoweB b = new PrzestawieniaMacierzoweB();
-            String tab = b.Cipher("To jest szyfr i nie wiesz co tu jest napisane");
-            String result = b.Decipher("Tywui rejas  t0j sentnc 0ofi s ion0eizsese a0z tp0");
+            //String tab = b.Cipher("To jest szyfr i nie wiesz co tu jest napisane","afbdhcegij");
+            //String result = b.Decipher("Tywui rejas  t0j sentnc 0ofi s ion0eizsese a0z tp0", "afbdhcegij");
+            String tab = b.Cipher("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE");
+            String result = b.Decipher("HECRN CEYI ISEP SGDI RNTO AAES RMPN SSRO EEBT ETIA EEHS", "CONVENIENCE");
             ViewBag.tab = tab;
             ViewBag.result = result;
             return View();
