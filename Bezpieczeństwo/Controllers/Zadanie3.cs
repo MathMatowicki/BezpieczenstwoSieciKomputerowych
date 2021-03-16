30386ce5516589bee89034ee76d9e4ccec4435ed
ewa <ewcia112211@gmail.com>
2021-03-16 09:59:28 +0100

    Szyfr strumieniowy - widok

---
+++ Bezpieczeństwo/Controllers/Zadanie3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bezpieczeństwo.Controllers
+{
+    public class Zadanie3 : Controller
+    {
+        private readonly ILogger<Zadanie3> _logger;
+        private readonly IHostingEnvironment _env;
+        public Zadanie3(ILogger<Zadanie3> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Algorithms()
+        {
+            List<String> result = new List<string>();
+            ViewBag.result = result;
+            return View();
+        }
+
+
+
+
+
+    }
+}
