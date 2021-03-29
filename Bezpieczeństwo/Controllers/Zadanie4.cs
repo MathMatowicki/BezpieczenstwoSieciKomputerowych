d04f9ddef05be047e06fd4c90f348ddde4c54223
Gabrysia Sawicka <99sgabi@wp.pl>
2021-03-29 15:42:37 +0200

    constants

---
+++ Bezpieczeństwo/Controllers/Zadanie4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bezpieczeństwo.Controllers
+{
+    public class Zadanie4 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Algorithms()
+        {
+            return View();
+        }
+    }
+}
